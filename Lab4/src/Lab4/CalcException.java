22ac2110f8f54603a0c3ba5b7083ab7d35a3147b
Ivan Chychkan <106067725+NicePancakes@users.noreply.github.com>
2023-11-05 16:40:29 +0200

    Update lab4

--- Lab4/src/Lab4/CalcException.java
+++ Lab4/src/Lab4/CalcException.java
@@ -5,13 +5,6 @@ public class CalcException extends ArithmeticException {
   }
 
   public CalcException(String cause) {
-    // String prefix = cause.startsWith("[1]", 0)
-    //     ? "first prefix"
-    //     : cause.startsWith("[2]", 0)
-    //         ? "second prefix"
-    //         : cause.startsWith("[3]", 0)
-    //             ? "third prefix"
-    //             : "unknown prefix";
 
     super("[ cause ] " + cause);
   }
